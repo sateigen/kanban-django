062a1432c0dc9a8f8fd70101b7fb5ac598627013
sateigen <shannon.teigen@gmail.com>
2016-08-23 16:57:28 -0400

    Working views for Board, Task, and Ticket models

--- kanban/board/views.py
+++ kanban/board/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Board, Task, Ticket
+from .serializers import BoardSerializer, TaskSerializer, TicketSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+class BoardViewSet(viewsets.ModelViewSet):
+    serializer_class = BoardSerializer
+    queryset = Board.objects.all()
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    serializer_class = TicketSerializer
+    queryset = Ticket.objects.all()
--- kanban/kanban/urls.py
+++ kanban/kanban/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+from board import views
+
+
+router = routers.DefaultRouter()
+router.register(r'board', views.BoardViewSet)
+router.register(r'ticket', views.TicketViewSet)
+router.register(r'task', views.TaskViewSet)
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(router.urls)),
     url(r'^', include('board.urls'))
 ]
